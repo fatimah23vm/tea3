d3e8f9b140930c840a72f95e819d0fd11ce6e50a
fatimah23vm <ifatemah.23@icloud.com>
2023-10-05 12:50:33 +0300

    Initial Commit

---
+++ tea3/tea3App.swift
@@ -0,0 +1,17 @@
+//
+//  tea3App.swift
+//  tea3
+//
+//  Created by Fatimah Adeeb on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct tea3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
